a1fd6e4bda29b71d51ce55c41c584123efb2dd14
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2020-07-20 19:03:13 +0200

    interfaces/bluez: allow clients access to audio streams
    Let clients like pulseaudio access audio streams from bluetooth
    devices.

--- interfaces/builtin/bluez.go
+++ interfaces/builtin/bluez.go
@@ -186,6 +186,9 @@ dbus (receive)
     path=/org/bluez{,/**}
     interface=org.freedesktop.DBus.*
     peer=(label=unconfined),
+
+# Allow access to bluetooth audio streams
+network bluetooth,
 `
 
 const bluezPermanentSlotSecComp = `
