c293023d02334706f85505dfa0b0d706e955c250
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-03-03 11:06:02 +0000

    gadget/install: use info from gadget in diskWithSystemSeed
    As we do not need the full layout for this function.

--- gadget/install/install_test.go
+++ gadget/install/install_test.go
@@ -518,7 +518,7 @@ func (s *installSuite) TestDeviceFromRoleHappy(c *C) {
 	lv, err := gadgettest.LayoutFromYaml(c.MkDir(), mockUC20GadgetYaml, uc20Mod)
 	c.Assert(err, IsNil)
 
-	device, err := install.DiskWithSystemSeed(lv)
+	device, err := install.DiskWithSystemSeed(lv.Volume)
 	c.Assert(err, IsNil)
 	c.Check(device, Equals, "/dev/fakedevice0")
 }
@@ -528,7 +528,7 @@ func (s *installSuite) TestDeviceFromRoleErrorNoMatchingSysfs(c *C) {
 	lv, err := gadgettest.LayoutFromYaml(c.MkDir(), mockUC20GadgetYaml, uc20Mod)
 	c.Assert(err, IsNil)
 
-	_, err = install.DiskWithSystemSeed(lv)
+	_, err = install.DiskWithSystemSeed(lv.Volume)
 	c.Assert(err, ErrorMatches, `cannot find device for role system-seed: device not found`)
 }
 
@@ -537,7 +537,7 @@ func (s *installSuite) TestDeviceFromRoleErrorNoRole(c *C) {
 	lv, err := gadgettest.LayoutFromYaml(c.MkDir(), mockGadgetYaml, nil)
 	c.Assert(err, IsNil)
 
-	_, err = install.DiskWithSystemSeed(lv)
+	_, err = install.DiskWithSystemSeed(lv.Volume)
 	c.Assert(err, ErrorMatches, "cannot find role system-seed in gadget")
 }
 
