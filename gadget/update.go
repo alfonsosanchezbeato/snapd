e95889d556547ac8c4ab209fa9b5dc6930b66b74
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-04-13 08:29:39 +0100

    gadget: change function checking offset range to return an error
    So we can print easily extra information.

--- gadget/update.go
+++ gadget/update.go
@@ -349,16 +349,8 @@ func EnsureVolumeCompatibility(gadgetVolume *Volume, diskVolume *OnDiskVolume, o
 		}
 
 		// start offset mismatch
-		if !IsValidStartOffset(ds.StartOffset, vss, vssIdx) {
-			sOffMin := minStructureOffset(vss, vssIdx)
-			sOffMax := maxStructureOffset(vss, vssIdx)
-			maxDesc := "unbounded"
-			if sOffMax != UnboundedStructureOffset {
-				maxDesc = fmt.Sprintf("%d (%s)", sOffMax, sOffMax.IECString())
-			}
-			return false, fmt.Sprintf("start offset not in the valid interval (disk: %d (%s) and gadget: min: %d (%s): max: %s)",
-				ds.StartOffset, ds.StartOffset.IECString(),
-				sOffMin, sOffMin.IECString(), maxDesc)
+		if err := CheckValidStartOffset(ds.StartOffset, vss, vssIdx); err != nil {
+			return false, fmt.Sprintf("disk partition %q %v", ds.Name, err)
 		}
 
 		switch {
